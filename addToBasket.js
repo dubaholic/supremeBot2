f0a62e9e7b635aff394921f6f9561812775f640e
Jens Rosseau <jens.rosseau@gmail.com>
2020-05-26 10:37:20 +0200

    Added options

--- addToBasket.js
+++ addToBasket.js
@@ -1,38 +1,80 @@
 var url = window.location.href;
 var i;
 
-var category = "accessories";
-var item_name = "Tagless Tees";
-var item_color = "Black";
-var item_size = "Medium";
+//item info
+var category = "";
+var item_name = "";
+//Leave blank if you don't know the item color
+var item_color = "";
+//Leave blank if that item has no size
+var item_size = "";
 
-var fullName = "Jens Rosseau";
-var email = "jens.rosseau@gmail.com";
-var telephoneNumber = "0496919901";
-var address = "Krokusstraat 20";
-var city = "Merksem";
-var zipCode = "2170";
-var countryCode = "BE";
+//Personal info
+var fullName = "";
+var email = "";
+var telephoneNumber = "";
+var address = "";
+var address2 = "";
+var address3 = "";
+var city = "";
+var zipCode = "";
+var countryCode = "";
+
+//Payment options
 var paymentOption = "paypal";
+var creditcardNumber = "";
+var creditcardMonth = "";
+var creditcardYear = "";
+var creditcardCvv = "";
 
+// var creditcardNumber = "";
+// var creditcardMonth = "";
+// var creditcardYear = "";
+// var creditcardCvv = "";
+
+//Delay options
+var cartItemDelay = 1900;
+var checkoutDelay = 5000;
 
 function fillCheckOut() {
+
+    //Fill in the personal info
     document.getElementById("order_billing_name").value = fullName;
     document.getElementById("order_email").value = email;
     document.getElementById("order_tel").value = telephoneNumber;
     document.getElementById("bo").value = address;
+    document.getElementById("oba3").value = address2;
+    document.getElementById("order_billing_address_3").value = address3;
     document.getElementById("order_billing_city").value = city;
     document.getElementById("order_billing_zip").value = zipCode;
     document.getElementById("order_billing_country").value = countryCode;
+
+    //Fill in all the CC info
     document.getElementById("credit_card_type").value = paymentOption;
+    if (paymentOption != "paypal") {
+        document.getElementById("cnb").value = creditcardNumber;
+        document.getElementById("credit_card_month").value = creditcardMonth;
+        document.getElementById("credit_card_year").value = creditcardYear;
+        document.getElementById("vval").value = creditcardCvv;
+    }
+
     document.getElementsByClassName("iCheck-helper")[1].click();
+
     var timeEnd = new Date;
     localStorage.setItem("endTime", timeEnd.getSeconds());
-    setTimeout(document.getElementsByName("commit")[0].click(), 2100);
+
+    setTimeout(clickCheckout, 1900);
+
+}
+
+function clickCheckout() {
+    var timeEnd = new Date;
+    localStorage.setItem("endTime2", timeEnd.getSeconds());
+    document.getElementsByName("commit")[0].click()
 }
 
 function pickCategory() {
-    chrome.storage.sync.get('category', function (data) {
+    chrome.storage.sync.get('selectedCategory', function (data) {
         var redirect = "https://www.supremenewyork.com/shop/all/";
         var replace = redirect + category;
         chrome.runtime.sendMessage({ redirect: replace });
@@ -42,12 +84,18 @@ function pickCategory() {
 function pickItem() {
     chrome.storage.sync.get('item_name', function (data) {
         var items = document.getElementsByClassName('inner-article');
-
         for (i = 0; i < items.length; i++) {
-            console.log(items[i].innerHTML);
+            //If both item name and color are known
             if ((items[i].innerHTML).includes(item_name) && (items[i].innerHTML).includes(item_color)) {
-                console.log(items[i].getElementsByClassName("name-link")[0].href);
-                var itemUrl =items[i].getElementsByClassName("name-link")[0].href;
+                var itemUrl = items[i].getElementsByClassName("name-link")[0].href;
+                localStorage.setItem("itemUrl", itemUrl);
+                chrome.runtime.sendMessage({ redirect: itemUrl });
+                cartItem(itemUrl);
+                break;
+            } 
+            //Check if only item name is known
+            else if ((items[i].innerHTML).includes(item_name)){
+                var itemUrl = items[i].getElementsByClassName("name-link")[0].href;
                 localStorage.setItem("itemUrl", itemUrl);
                 chrome.runtime.sendMessage({ redirect: itemUrl });
                 cartItem(itemUrl);
@@ -58,18 +106,29 @@ function pickItem() {
 }
 
 function cartItem() {
-    chrome.storage.sync.get('item_size', function (data) { 
-        var options = document.getElementById("size").options;
-
-        for (i = 0; i < options.length; i++) {
-            console.log(options[i]);
-            if ((options[i].innerHTML).includes(item_size) && !(options[i].innerHTML).includes("XLarge") ) {
-                document.getElementById("size").value = options[i].value;
-                document.getElementsByName("commit")[0].click();
-                setTimeout(redirectToCheckOut, 2200);
+    chrome.storage.sync.get('item_size', function (data) {
+        if (item_size != "") {
+            var options = document.getElementById("size").options;
+            for (i = 0; i < options.length; i++) {
+                if ((options[i].innerHTML).includes(item_size) && !(options[i].innerHTML).includes("XLarge")) {
+                    document.getElementById("size").value = options[i].value;
+                    document.getElementsByName("commit")[0].click();
+                    setTimeout(redirectToCheckOut, 1500);
+                }
             }
         }
+        else 
+        {
+            document.getElementsByName("commit")[0].click();
+            setTimeout(redirectToCheckOut, 1500);
+        }
     });
+}
+
+function checkAvailability() {
+    while (document.getElementById("add-remove-buttons").includes("sold out")) {
+        setTimeout(location.reload(), 3000);
+    }
 }
 
 function redirectToCheckOut() {
--- options.js
+++ options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
     var category = document.getElementById('category').value;
-    var name = document.getElementById('itemName').value;
     chrome.storage.sync.set({
       selectedCategory: category,
-      itemName: name
     }, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
@@ -20,10 +18,8 @@ function save_options() {
   function restore_options() {
     chrome.storage.sync.get({
       selectedCategory: 'jackets',
-      itemName: ''
     }, function(items) {
       document.getElementById('category').value = items.selectedCategory;
-      document.getElementById('itemName').value = items.itemName;
     });
   }
   document.addEventListener('DOMContentLoaded', restore_options);
